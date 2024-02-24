179cd17eacffa58289353e8d3542e4e2598995df
AlexCrisTin <153697285+AlexCrisTin@users.noreply.github.com>
2024-02-24 09:14:42 +0000

    Assignment is Tricky Part 1

---
+++ src/main/java/com/mycompany/app/EX/bt2.java
@@ -0,0 +1,23 @@
+package com.mycompany.app.EX;
+import java.util.Random;
+public class bt2 {
+     public static void main(String[] args) {
+        Random random = new Random();
+        for(int i = 0; i <= 25; i++){
+        int num1 = random.nextInt(100)+1;
+        int num2 = random.nextInt(100)+1;
+            for(int j = 0; j < 25; j++){
+            if(num1 < num2){
+                System.out.println("Num1 < Num2");
+            }
+            if(num1 > num2){
+                System.out.println("Num1 > Num2");
+            }
+            else
+                System.err.println("Num1 = Num2");
+        }
+        }
+        
+    }
+}
+
--- src/main/java/com/mycompany/app/Number.java
+++ src/main/java/com/mycompany/app/Number.java
@@ -5,8 +5,6 @@
 
 package com.mycompany.app;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello Remote World!");
-    }
+class Number {
+    public int i;
 }
