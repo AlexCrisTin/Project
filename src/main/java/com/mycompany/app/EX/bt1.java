6bb89b16a8fccfb7469647a00654f062e4fd7a07
AlexCrisTin <nholaian2222@gmail.com>
2024-02-28 16:39:14 +0700

    bt1

--- src/main/java/com/mycompany/app/EX/bt1.java
+++ src/main/java/com/mycompany/app/EX/bt1.java
@@ -1,7 +1,7 @@
 package com.mycompany.app.EX;
 public class bt1 {
     public static void main(String[] args) {
-        int n = 100;
+       
         for(int i = 0; i <= 100; i++){
             System.out.println(i);
         }
