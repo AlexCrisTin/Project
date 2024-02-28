a2a0ca16e4d4bfe6cde5a6c13acb25d4289d814b
AlexCrisTin <nholaian2222@gmail.com>
2024-02-28 17:46:18 +0700

    bt7

--- src/main/java/com/mycompany/app/EX_Tuan_1/bt7.java
+++ src/main/java/com/mycompany/app/EX_Tuan_1/bt7.java
@@ -2,7 +2,6 @@ package com.mycompany.app.EX_Tuan_1;
 
 public class bt7 {
     public static void main(String[] args) {
-        int n = 100;
         for(int i = 0; i <= 100; i++){
             System.out.println(i);
             if(i == 99){
