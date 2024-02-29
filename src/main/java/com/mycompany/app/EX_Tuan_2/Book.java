8a5805102f5fb6aaf7d1804a52bb2f7a54e05e63
AlexCrisTin <nholaian2222@gmail.com>
2024-03-01 00:15:03 +0700

    EX

--- src/main/java/com/mycompany/app/EX_Tuan_2/Book.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Book.java
@@ -1,6 +1,14 @@
 package com.mycompany.app.EX_Tuan_2;
 
 public class Book {
+
+
+
+
+
+
+
+    
     public static void main(String[] args) {
         
     }
---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Initialization_and_Cleanup/EX1.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.EX_Tuan_2.Initialization_and_Cleanup;
+
+public class EX1 {
+   public static void PrintString(){
+        System.out.println("String"); 
+    }
+    public static void main(String[] args) {
+        PrintString();
+        PrintString();   
+    }
+}
