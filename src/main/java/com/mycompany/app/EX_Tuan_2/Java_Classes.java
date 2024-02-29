44bca31b01625122ebb05041b399428699f42ce7
AlexCrisTin <nholaian2222@gmail.com>
2024-02-29 17:29:09 +0700

    W3School

---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Java_Classes.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.EX_Tuan_2;
+
+public class Java_Classes {
+    public class Main {
+        int x = 5;
+      }
+      class Second {
+        public void main(String[] args) {
+          Main myObj = new Main();
+          System.out.println(myObj.x);
+        }
+      }
+}
--- src/main/java/com/mycompany/app/EX_Tuan_2/Java_method.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Java_method.java
@@ -1,5 +1,11 @@
 package com.mycompany.app.EX_Tuan_2;
 
 public class Java_method {
-    
-}
+    static void myMethod() {
+      System.out.println("I just got executed!");
+    }
+  
+    public static void main(String[] args) {
+      myMethod();
+    }
+  }
