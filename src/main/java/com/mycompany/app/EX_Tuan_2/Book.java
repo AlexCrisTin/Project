37e8b89ddd4705aa4ea46d4dbcc26c0f89506073
AlexCrisTin <nholaian2222@gmail.com>
2024-03-01 07:22:33 +0700

    Book

--- src/main/java/com/mycompany/app/EX_Tuan_2/Book.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Book.java
@@ -1,15 +1,17 @@
 package com.mycompany.app.EX_Tuan_2;
 
 public class Book {
-
-
-
-
-
-
-
-    
-    public static void main(String[] args) {
+        static String title;
+        static String author;
+        static int page;
+       
+        public Book(String t, String a, int p){
+            title = t;
+            author = a;
+            page = p;
+        }public static void main(String[] args) {
+        Book usefullBook = new Book("The Begin", "Mag", 69);
+        System.out.println("Title: " + Book.title + "\n" + "Author: " + Book.author + "\n" + "Page: " + Book.page);
         
     }
 }
