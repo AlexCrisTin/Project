16a87af8fab682cf5f7c471ad625447e3e5011f6
AlexCrisTin <nholaian2222@gmail.com>
2024-03-01 07:53:27 +0700

    time

--- src/main/java/com/mycompany/app/EX_Tuan_2/Time.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Time.java
@@ -1,7 +1,20 @@
 package com.mycompany.app.EX_Tuan_2;
 
 public class Time {
-    int hour;
-    int minute;
     int second;
+    int minute;
+    int hour;
+
+Time(){setTime(0,0,0);}
+private void setTime(int i, int j, int k) {
+    
+  
+}
+Time(int s){setTime(s,0,0);}
+Time(int s, int m){setTime(s,m, 0);}
+Time(int s, int m, int h){setTime(s,m,h);}
+
+
+
+
 }
