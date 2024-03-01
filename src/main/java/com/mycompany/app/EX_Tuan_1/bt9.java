f205211c41257c64898e80927864c2b049aed337
Minato Peko <hieuidentiny@gmail.com>
2024-03-01 08:24:23 +0700

    fak u 9

--- src/main/java/com/mycompany/app/EX_Tuan_1/bt9.java
+++ src/main/java/com/mycompany/app/EX_Tuan_1/bt9.java
@@ -3,7 +3,7 @@ package com.mycompany.app.EX_Tuan_1;
 
     public class bt9 {
         public static void main(String[] args) {
-            int n = 5;
+            int n = 20;
             int x = 1, y = 0;
             for(int i = 0; i < n; i++ ){
                 int z = x + y;
