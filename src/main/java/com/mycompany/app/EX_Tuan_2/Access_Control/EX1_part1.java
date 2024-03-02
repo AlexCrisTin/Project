80c052922c11d5e1ea94495927125d120e69ab1f
AlexCrisTin <nholaian2222@gmail.com>
2024-03-02 22:43:04 +0700

    Access control

---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX1_part1.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.EX_Tuan_2.Access_Control;
+
+public class EX1_part1 {
+   public String name;
+}
---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX1_part2.java
@@ -0,0 +1,12 @@
+package com.mycompany.app.EX_Tuan_2.Access_Control;
+
+public class EX1_part2 {
+    public static void main(String[] args) {
+        EX1_part1 n = new EX1_part1();
+        n.name = "Name";
+    
+        System.out.println(n.name);
+    }
+
+    
+}
