73a1ab175bd79f07c5945f052eeeb0a3d729ec60
Minato Peko <hieuidentiny@gmail.com>
2024-03-03 23:07:40 +0700

    brh

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,8 +1,23 @@
 package com.mycompany.app;
+
 import com.mycompany.app.EX_Tuan_2.Code_1.*;
 public class App {
     public static void main(String[] args) {
-        Code_1 temp=new Code_1();
-        temp.main();
+        System.out.println("Hello, World!");
+        System.out.println("******Book******");
+        Book temp = new Book(null, null, 0);
+        temp.main(args);
+        
+        System.out.println("******Time******");
+        Time t  = new Time();
+        t.main(args);
+
+        System.out.println("******Recursion******");
+        Recursion r  = new Recursion();
+        r.main(args);
+
+        System.out.println("******NameNumber******");
+        NameNumber n  = new NameNumber();
+        n.main(args);
     }
 }
