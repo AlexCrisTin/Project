04e2424c5f4fedf9d4e0873f5beeb109b8bfece1
AlexCrisTin <nholaian2222@gmail.com>
2024-03-03 17:40:19 +0700

    ex

--- src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX1_part2.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX1_part2.java
@@ -3,7 +3,7 @@ package com.mycompany.app.EX_Tuan_2.Access_Control;
 public class EX1_part2 {
     public static void main(String[] args) {
         EX1_part1 n = new EX1_part1();
-        n.name = "Name";
+        n.name = "Name here";
     
         System.out.println(n.name);
     }
