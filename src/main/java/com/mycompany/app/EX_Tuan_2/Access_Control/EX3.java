f7ed22c61c1e573f8134f2a5d6e14db9b14769a4
Minato Peko <hieuidentiny@gmail.com>
2024-03-04 00:06:18 +0700

    meh

--- src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX3.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX3.java
@@ -2,6 +2,6 @@ package com.mycompany.app.EX_Tuan_2.Access_Control;
 
 public class EX3 {
     public void debug(String s){
-		System.out.println(s + " debug ON");
+		System.out.println(s + " eh?");
 	}
 }
---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/Ex4_show.java
@@ -0,0 +1,8 @@
+package com.mycompany.app.EX_Tuan_2.Access_Control;
+
+public class Ex4_show {
+    public static void show2(){
+		Exercise4 ex4 = new Exercise4();
+		ex4.show();
+	}
+}
