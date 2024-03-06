35a903e480af04207d280e7d7872a34efede7101
Minato Peko <hieuidentiny@gmail.com>
2024-03-06 20:36:27 +0700

    ex7-8

---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/Ex7_out.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.EX_Tuan_2.Access_Control;
+
+public class Ex7_out {
+    public static void main(String[] args) {
+        Ex7 ex7 = new Ex7();
+}
+}
---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/Ex8.java
@@ -0,0 +1,5 @@
+package com.mycompany.app.EX_Tuan_2.Access_Control;
+
+public class Ex8 {
+    
+}
