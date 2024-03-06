3acf9db0a04b361d30873f967e454d3d659bc006
Minato Peko <hieuidentiny@gmail.com>
2024-03-06 23:15:22 +0700

    fix

--- src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX2.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/EX2.java
@@ -1,5 +1,14 @@
 package com.mycompany.app.EX_Tuan_2.Access_Control;
 
+import java.util.*;
+import Vector.java;
+public class Vector {
+	public Vector() {
+	System.out.println("net.mindview.simple.Vector");
+	}
+
+
+}
 public class EX2 {
 	public static void main(String[] args){ 
 	}
--- src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/Ex8.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Access_Control/Ex8.java
@@ -1,5 +1,40 @@
 package com.mycompany.app.EX_Tuan_2.Access_Control;
 
+class Connection{
+	private Connection(int n){
+			System.out.println("Creating connection " + n);
+		}
+	public static Connection makeConnection(int i){ 
+		return new Connection(i);
+	}
+	public void connect(int a){
+		System.out.println("Connecting to socket " + a);
+	}
+}
+
+class ConnectionManager{
+	private static int conncount=0;
+	private static Connection[] connmgr = new Connection[5];
+	private static void connInit(){
+			for (int i=0; i<5; i++) 
+			connmgr[i] = Connection.makeConnection(i);
+	}
+	private ConnectionManager(){ 
+		connInit();
+	}
+	private static ConnectionManager cm = new ConnectionManager();
+	
+	public static Connection access(){
+		if (conncount<5){
+			System.out.println("Accessing connection " + conncount);
+			return connmgr[conncount++];
+			}
+		else return null;
+	}
+}
 public class Ex8 {
-    
+    public static void main(String[] args){
+		for (int n=0; n<7; n++)
+			ConnectionManager.access().connect(n);
+	}
 }
