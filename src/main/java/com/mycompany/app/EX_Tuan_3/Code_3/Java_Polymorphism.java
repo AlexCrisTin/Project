37618dfc8191e95690cc416bf47d9ca1555598d3
AlexCrisTin <nholaian2222@gmail.com>
2024-03-07 17:00:17 +0700

    W3Schools

--- src/main/java/com/mycompany/app/EX_Tuan_3/Code_3/Java_Polymorphism.java
+++ src/main/java/com/mycompany/app/EX_Tuan_3/Code_3/Java_Polymorphism.java
@@ -1,30 +1,19 @@
 package com.mycompany.app.EX_Tuan_3.Code_3;
 
 class Animal {
-    public void animalSound() {
-      System.out.println("The animal makes a sound");
-    }
+  public void animalSound() {
+    System.out.println("The animal makes a sound");
   }
-  
-  class Pig extends Animal {
-    public void animalSound() {
-      System.out.println("The pig says: wee wee");
-    }
+}
+
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
   }
-  
-  class Dog extends Animal {
-    public void animalSound() {
-      System.out.println("The dog says: bow wow");
-    }
+}
+
+class Dog extends Animal {
+  public void animalSound() {
+    System.out.println("The dog says: bow wow");
   }
-  
-  class Main {
-    public static void main(String[] args) {
-      Animal myAnimal = new Animal();  // Create a Animal object
-      Animal myPig = new Pig();  // Create a Pig object
-      Animal myDog = new Dog();  // Create a Dog object
-      myAnimal.animalSound();
-      myPig.animalSound();
-      myDog.animalSound();
-    }
-  }
+}
