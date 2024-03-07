8c4ec728d7aaed8fd64d3b7e47a6e419352c9e32
Minato Peko <hieuidentiny@gmail.com>
2024-03-07 18:20:26 +0700

    ex2 done

--- src/main/java/com/mycompany/app/EX_Tuan_2/Reusing_Classess/EX1.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Reusing_Classess/EX1.java
@@ -1,5 +1,18 @@
 package com.mycompany.app.EX_Tuan_2.Reusing_Classess;
 
-public class EX1 {
-    
+class Simple{
+	void alloy(){
+		System.out.println(" ");
+	}
+}
+public class Ex1 {
+	 Simple smpl;
+	void useSimple(){
+		smpl = new Simple();
+		smpl.alloy();
+	}
+	public static void main(String[] args){
+		Ex1 ex1 = new Ex1();
+		ex1.useSimple();
+	}
 }
