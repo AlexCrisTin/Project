2baf328da04131bd5e52e8f4454d236ad0cf6dae
AlexCrisTin <nholaian2222@gmail.com>
2024-03-08 09:01:14 +0700

    clean

--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Initialization_and_Cleanup/EX12.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Initialization_and_Cleanup/EX12.java
@@ -1,22 +1,22 @@
 package com.mycompany.app.EX_Tuan_2.Code_2.Initialization_and_Cleanup;
 
 public class EX12 {
-    boolean isfilled;
+    boolean isFilled;
     class Tank{
-        boolean isfilled = false;
-        Tank(boolean Filled) {
-            Filled = isfilled;
-            } 
+        boolean isFilled = false;
+        Tank(boolean filled) {
+            this.isFilled = filled;
+        } 
     }
     public void fill() {
-       boolean isfilled = true;
+       isFilled = true;
     }
     public void empty() {
-       boolean isfilled = false;
+       isFilled = false;
     }
     @Override
     protected void finalize() {
-        if (isfilled) {
+        if (isFilled) {
             System.out.println("Error: Tank is not empty during cleanup!");
         }
     }
@@ -27,6 +27,7 @@ public class EX12 {
     System.gc();
         EX12 tankdiamon = new EX12();
     tankdiamon.fill();
-    
+    tankcopper = null;
+    System.gc();
     }
 }
