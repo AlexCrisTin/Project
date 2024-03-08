a573af6fa0111faaa40235e689d33dec290ccdf7
Minato Peko <hieuidentiny@gmail.com>
2024-03-08 08:51:59 +0700

    Ex6

---
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Reusing_Classess/Ex6.java
@@ -0,0 +1,29 @@
+package com.mycompany.app.EX_Tuan_2.Code_2.Reusing_Classess;
+
+class Game {
+	Game(int i) {
+		System.out.println("Game constructor");
+	}
+}
+
+class BoardGame extends Game {
+	BoardGame(int i) {
+		
+		super(i);		
+		System.out.println("BoardGame constructor");
+	}
+}
+
+class Chess extends BoardGame {
+	Chess() {
+		super(11);
+		System.out.println("Chess constructor");
+	}
+} 
+
+public class Ex6{
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		Chess x = new Chess();
+		}
+}
--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Reusing_Classess/Game.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.app.EX_Tuan_2.Code_2.Reusing_Classess;
-
-public class Game {
-
-    public Game(int i) {
-        //TODO Auto-generated constructor stub
-    }
-
-}
