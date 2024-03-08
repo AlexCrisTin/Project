5d649e4e23ad370931f9f767e31939152892b4b5
Minato Peko <hieuidentiny@gmail.com>
2024-03-08 07:50:12 +0700

    chill friend

--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Access_Control/EX1_part1.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Access_Control/EX1_part1.java
@@ -1,6 +1,6 @@
 package com.mycompany.app.EX_Tuan_2.Code_2.Access_Control;
 
-public class EX1_part1 {  //A class. In a package. For a gentleman.
+public class EX1_part1 {  
 	public static void main(String[] args){ 
 		System.out.println(" ");
 	}
