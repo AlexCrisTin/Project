afe92b34d4325df8febfeb99ccf4b78753d81f07
AlexCrisTin <nholaian2222@gmail.com>
2024-03-08 07:18:18 +0700

    clean

--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Initialization_and_Cleanup/EX9.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Initialization_and_Cleanup/EX9.java
@@ -1,6 +1,21 @@
 package com.mycompany.app.EX_Tuan_2.Code_2.Initialization_and_Cleanup;
 
 public class EX9 {
-    
-}
+    String name;
+    public EX9() {
+        this("Overload"); 
+    }
+    public EX9(String name) {
+        this.name = name;
+    }
+    public void display() {
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        EX9 out1 = new EX9();
+        out1.display();
 
+        EX9 out2 = new EX9("Another overload");
+        out2.display(); 
+    }
+}
--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Reusing_Classess/Ex3.java
+++
@@ -1,5 +0,0 @@
-package com.mycompany.app.EX_Tuan_2.Code_2.Reusing_Classess;
-
-public class Ex3 {
-    
-}
