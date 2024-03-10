07fca1509e564b512df0f9154f7daff446f99a0c
AlexCrisTin <nholaian2222@gmail.com>
2024-03-10 15:32:14 +0700

    clean

--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Initialization_and_Cleanup/EX15.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Initialization_and_Cleanup/EX15.java
@@ -3,7 +3,7 @@ package com.mycompany.app.EX_Tuan_2.Code_2.Initialization_and_Cleanup;
 public class EX15 {
     String string;
     {
-        string = "This is a string using instance  initialization";
+        string = "This is a string using instance initialization";
     }
     public String MyString() {
         return string;
