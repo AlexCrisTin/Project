3fbf6194c95ced0d2e043a39f2481f0c2f7821c6
AlexCrisTin <nholaian2222@gmail.com>
2024-03-10 21:03:24 +0700

    poly

--- src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Polymorphism/EX1.java
+++ src/main/java/com/mycompany/app/EX_Tuan_2/Code_2/Polymorphism/EX1.java
@@ -1,5 +1,40 @@
 package com.mycompany.app.EX_Tuan_2.Code_2.Polymorphism;
-
-public class EX1 {
+    class Cycle{
+        void ride(){
+            System.out.println("normal cycle");
+        }
+    }
+    class Unicycle extends Cycle{
+        void ride(){
+            System.out.println("unicycle");
+        }
+    }
+    class Bicycle extends Cycle{
+        void ride(){
+            System.out.println("bicycle");
+        }
+    }
+    class Tricycle extends Cycle{
+        void ride(){
+            System.out.println("tricycle");
+        }
     
+    
+
+public static void main(String[] args) {
+      
+        Cycle basicCycle = new Cycle();
+        Unicycle unicycle = new Unicycle();
+        Bicycle bicycle = new Bicycle();
+        Tricycle tricycle = new Tricycle();
+
+        Cycle upcastUnicycle = unicycle;
+        Cycle upcastBicycle = bicycle;
+        Cycle upcastTricycle = tricycle;
+
+        basicCycle.ride();
+        upcastUnicycle.ride();
+        upcastBicycle.ride();
+        upcastTricycle.ride();
+    }
 }
