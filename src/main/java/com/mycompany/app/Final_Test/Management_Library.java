22e6246d2bebd81a3bbdb33afea66f25927f95f6
AlexCrisTin <nholaian2222@gmail.com>
2024-03-26 21:52:58 +0700

    gg

--- src/main/java/com/mycompany/app/Final_Test/Management_Library.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.app.Final_Test;
-import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
-public class Management_Library {
-    
-}
