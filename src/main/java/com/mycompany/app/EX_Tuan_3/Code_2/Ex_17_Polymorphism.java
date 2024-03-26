1923b64c3517ac7e3a54f3762854a3266a9444b8
AlexCrisTin <nholaian2222@gmail.com>
2024-03-26 21:54:57 +0700

    gg

--- src/main/java/com/mycompany/app/EX_Tuan_3/Code_2/Ex_17_Polymorphism.java
+++ src/main/java/com/mycompany/app/EX_Tuan_3/Code_2/Ex_17_Polymorphism.java
@@ -1,5 +1,47 @@
 package com.mycompany.app.EX_Tuan_3.Code_2;
 
 public class Ex_17_Polymorphism {
+    class Cycles{
+	
+    }
     
+    class Unicycles extends Cycles{
+        public String toString(){
+        return "Unicycles";	
+        }
+        public void balance(){
+            System.out.println("Balancing on " + this);
+        }
+    }
+    
+    class Bicycles extends Cycles{
+        public String toString(){
+            return "Bicycles";
+        }
+        public void balance(){
+            System.out.println("Balancing on " + this);
+            }
+    }
+        
+    class Tricycles extends Cycles{
+        public String toString(){
+            return "Tricycles";	
+        }
+    }
+        
+    public class Ex17 {
+        public void ride(Cycles c){
+            System.out.println("Riding a " + c.toString());
+        }
+        public void main(String[] args) {
+            Cycles[] c = {
+                    new Unicycles(), 
+                    new Bicycles(),
+                    new Tricycles()
+            };
+            
+            ((Unicycles)c[0]).balance();
+            ((Bicycles)c[1]).balance();
+        }
+    }
 }
